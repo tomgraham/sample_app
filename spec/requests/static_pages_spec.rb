dce592829a1c2dc05496a48cc6774f6cf48e365b
Tom Graham <jtgraham@gmail.com>
2012-01-24 10:50:20 -0600

    Add tests for contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -14,6 +14,20 @@ describe "Static pages" do
     end
   end
   
+  describe "Contact page" do   
+    it "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Sample App')
+    end
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title',
+              :text => "Ruby on Rails Tutorial Sample App | Contact")
+    end
+  end 
+  
+  
+  
   describe "Help page" do
       it "should have the h1 'Help'" do
         visit '/static_pages/help'
