c39ba91cb4f4bb69ac76246868e113d98aa47f4c
Tom Graham <jtgraham@gmail.com>
2012-01-24 07:04:32 -0600

    Add titles for home, about, and help pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -31,11 +31,12 @@ describe "Static pages" do
         visit '/static_pages/about'
         page.should have_selector('h1', :text => 'About Us')
       end
-      it "should have the title 'About'" do
-        visit '/static_pages/help'
+      it "should have the title 'About Us'" do
+        visit '/static_pages/about'
         page.should have_selector('title',
-                :text => "Ruby on Rails Tutorial Sample App | About")
+                :text => "Ruby on Rails Tutorial Sample App | About Us")
       end
     end
+
   
 end
