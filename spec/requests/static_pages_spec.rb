cd72720a7c60abf35edec5e08fdeb67da9e253a7
Tom Graham <jtgraham@gmail.com>
2012-01-24 11:03:45 -0600

    Add contact page.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,7 +5,8 @@ describe "Static pages" do
   describe "Home page" do   
     it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Sample App')
+      page.should have_selector('h1', 
+              :text => 'Sample App')
     end
     it "should have the title 'Home'" do
       visit '/static_pages/home'
@@ -17,7 +18,8 @@ describe "Static pages" do
   describe "Contact page" do   
     it "should have the h1 'Contact'" do
       visit '/static_pages/contact'
-      page.should have_selector('h1', :text => 'Sample App')
+      page.should have_selector('h1', 
+              :text => 'Contact')
     end
     it "should have the title 'Contact'" do
       visit '/static_pages/contact'
