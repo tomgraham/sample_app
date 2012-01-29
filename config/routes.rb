e2938a3b9d5979a54e831b018f32374f773eee69
Tom Graham <jtgraham@gmail.com>
2012-01-29 12:11:32 -0600

    Add Users page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 SampleApp::Application.routes.draw do
   
-  root :to => 'static_pages#home'
+  get "users/new"
+
+  root :to => "static_pages#home"
   
-  match '/help',    to: 'static_pages#help'
-  match '/about',   to: 'static_pages#about'
-  match '/contact', to: 'static_pages#contact'
+  match "/about",   to: "static_pages#about"
+  match "/contact", to: "static_pages#contact"
+  match "/help",    to: "static_pages#help"
+  match "/signup",  to: "users#new"
   
   
   get "static_pages/home"
